21341a1a33f5c8d38175c1b9f585075c73b648c7
alisndb <omarovali6@mail.ru>
2022-10-23 19:30:02 +0300

    Changed hw-1 files

--- hw-1/submission_template01.py
+++ hw-1/submission_template01.py
@@ -4,14 +4,12 @@ from torch import nn
 
 
 def create_model():
-    # Linear layer mapping from 784 features, so it should be 784->256->16->10
     linear_relu_stack = nn.Sequential(
         nn.Linear(784, 256, bias=True),
         nn.ReLU(),
         nn.Linear(256, 16, bias=True),
         nn.ReLU(),
-        nn.Linear(16, 10, bias=True),
-        nn.ReLU()
+        nn.Linear(16, 10, bias=True)
     )
 
     return linear_relu_stack
